05ba2374deb7d2aae98e42b6db7eed16902ed5ef
legokidlogan <26103433+legokidlogan@users.noreply.github.com>
2023-09-03 21:59:45 -0600

    Tweak convars

--- README.md
+++ README.md
@@ -11,12 +11,12 @@
 | cfc_shaped_charge_blastrange | The damage range the explosion has. | 100 |
 | cfc_shaped_charge_tracerange | The range the prop breaking explosion has. | 100 |
 | cfc_parachute_fall_speed | Target fall speed while in a parachute. | 200 |
-| cfc_parachute_fall_lerp | How quickly a parachute will reach its target fall speed. | 0.85 |
+| cfc_parachute_fall_lerp | How quickly a parachute will reach its target fall speed. | 0.97 |
 | cfc_parachute_horizontal_speed | How quickly you move in a parachute. | 80 |
 | cfc_parachute_horizontal_speed_unstable | How well you can control a parachute while holding another weapon. | 40 |
 | cfc_parachute_horizontal_speed_limit | Max horizontal speed of a parachute. | 700 |
 | cfc_parachute_sprint_boost | How much of a horizontal boost you get in a parachute while sprinting. | 1.25 |
-| cfc_parachute_handling | Improves parachute handling by making it easier to brake or chagne directions. 1 gives no handling boost, 0-1 reduces handling. | 4.5 |
+| cfc_parachute_handling | Improves parachute handling by making it easier to brake or chagne directions. 1 gives no handling boost, 0-1 reduces handling. | 4 |
 | cfc_parachute_destabilize_min_gap | Minimum time between direction changes while a parachute destabilizes, in seconds. | 0.3 |
 | cfc_parachute_destabilize_max_gap | Maximum time between direction changes while a parachute destabilizes, in seconds. | 2 |
 | cfc_parachute_destabilize_max_direction_change | Maximum angle change in a parachute's direction while it is destabilized. | 30 |
@@ -30,7 +30,7 @@
 | cfc_parachute_lfs_eject_launch_bias | How many degrees the LFS eject launch should course-correct the player's trajectory to send them straight up, for if their plane is tilted. | 25 |
 | cfc_parachute_lfs_eject_stability_time | How many seconds a player is immune to parachute instability when they launch out of an LFS plane. | 5 |
 | cfc_parachute_lfs_enter_radius | How close a player must be to enter an LFS if they are in a parachute and regular use detection fails. Makes it easier to get inside of an LFS for performing a Rendezook. | 800 |
-| cfc_parachute_space_equip_speed | The minimum falling speed required for a player to space-equip a parachute. | 450 |
+| cfc_parachute_space_equip_speed | The minimum falling speed required for a player to space-equip a parachute. | 370 |
 
 ## Client Convars
 
--- lua/cfc_pvp_weapons/shared/sh_parachute_convars.lua
+++ lua/cfc_pvp_weapons/shared/sh_parachute_convars.lua
@@ -1,10 +1,10 @@
 CreateConVar( "cfc_parachute_fall_speed", 200, { FCVAR_REPLICATED, FCVAR_ARCHIVE }, "Target fall speed while in a parachute.", 0, 50000 )
-CreateConVar( "cfc_parachute_fall_lerp", 0.95, { FCVAR_REPLICATED, FCVAR_ARCHIVE }, "How quickly a parachute will reach its target fall speed.", 0, 10 )
+CreateConVar( "cfc_parachute_fall_lerp", 0.97, { FCVAR_REPLICATED, FCVAR_ARCHIVE }, "How quickly a parachute will reach its target fall speed.", 0, 10 )
 CreateConVar( "cfc_parachute_horizontal_speed", 80, { FCVAR_REPLICATED, FCVAR_ARCHIVE }, "How quickly you move in a parachute.", 0, 50000 )
 CreateConVar( "cfc_parachute_horizontal_speed_unstable", 40, { FCVAR_REPLICATED, FCVAR_ARCHIVE }, "How well you can control a parachute while holding another weapon.", 0, 50000 )
 CreateConVar( "cfc_parachute_horizontal_speed_limit", 700, { FCVAR_REPLICATED, FCVAR_ARCHIVE }, "Max horizontal speed of a parachute.", 0, 50000 )
 CreateConVar( "cfc_parachute_sprint_boost", 1.25, { FCVAR_REPLICATED, FCVAR_ARCHIVE }, "How much of a horizontal boost you get in a parachute while sprinting.", 1, 10 )
-CreateConVar( "cfc_parachute_handling", 4.5, { FCVAR_REPLICATED, FCVAR_ARCHIVE }, "Improves parachute handling by making it easier to brake or chagne directions. 1 gives no handling boost, 0-1 reduces handling.", 0, 10 )
+CreateConVar( "cfc_parachute_handling", 4, { FCVAR_REPLICATED, FCVAR_ARCHIVE }, "Improves parachute handling by making it easier to brake or chagne directions. 1 gives no handling boost, 0-1 reduces handling.", 0, 10 )
 
 CreateConVar( "cfc_parachute_destabilize_min_gap", 0.3, { FCVAR_REPLICATED, FCVAR_ARCHIVE }, "Minimum time between direction changes while a parachute destabilizes, in seconds.", 0, 50000 )
 CreateConVar( "cfc_parachute_destabilize_max_gap", 2, { FCVAR_REPLICATED, FCVAR_ARCHIVE }, "Maximum time between direction changes while a parachute destabilizes, in seconds.", 0, 50000 )
@@ -15,7 +15,7 @@ CreateConVar( "cfc_parachute_destabilize_lurch_chance", 0.2, { FCVAR_REPLICATED,
 CreateConVar( "cfc_parachute_destabilize_shoot_lurch_chance", 0.1, { FCVAR_REPLICATED, FCVAR_ARCHIVE }, "The chance for an unstable parachute to lurch downwards when the player shoots a bullet.", 0, 1 )
 CreateConVar( "cfc_parachute_destabilize_shoot_change_chance", 0.25, { FCVAR_REPLICATED, FCVAR_ARCHIVE }, "The chance for an unstable parachute's direction to change when the player shoots a bullet.", 0, 1 )
 
-CreateConVar( "cfc_parachute_space_equip_speed", 350, { FCVAR_ARCHIVE, FCVAR_REPLICATED }, "The minimum falling speed required for a player to space-equip a parachute.", 0, 50000 )
+CreateConVar( "cfc_parachute_space_equip_speed", 370, { FCVAR_ARCHIVE, FCVAR_REPLICATED }, "The minimum falling speed required for a player to space-equip a parachute.", 0, 50000 )
 CreateConVar( "cfc_parachute_space_equip_sv", 1, { FCVAR_ARCHIVE, FCVAR_REPLICATED }, "Press spacebar while falling to quickly equip a parachute. Defines the default value for players.", 0, 1 )
 CreateConVar( "cfc_parachute_space_equip_double_sv", 0, { FCVAR_ARCHIVE, FCVAR_REPLICATED }, "Double tap spacebar to equip parachutes, instead of a single press. Defines the default value for players.", 0, 1 )
 CreateConVar( "cfc_parachute_space_equip_weapon_sv", 1, { FCVAR_ARCHIVE, FCVAR_REPLICATED }, "Instantly switch back to your previous weapon when space-equipping a parachute. Defines the default value for players.", 0, 1 )
