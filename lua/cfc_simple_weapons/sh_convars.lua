87da64007ab1764aa44ca8007bfa0b76196d7789
legokidlogan <26103433+legokidlogan@users.noreply.github.com>
2024-02-05 18:32:09 -0700

    Remove unused

--- lua/cfc_simple_weapons/sh_convars.lua
+++ lua/cfc_simple_weapons/sh_convars.lua
@@ -7,8 +7,4 @@ MinDamageMult = CreateConVar( "cfc_simple_weapons_min_damage_mult", 0.2, { FCVAR
 if CLIENT then
     SwayScale = CreateClientConVar( "cfc_simple_weapons_swayscale", 1, true, false, "The amount of viewmodel sway to apply to weapons" )
     BobScale = CreateClientConVar( "cfc_simple_weapons_bobscale", 1, true, false, "The amount of viewmodel bob to apply to weapons" )
-
-    VMOffsetX = CreateClientConVar( "cfc_simple_weapons_vm_offset_x", 0, true, false, "The forward/back offset to use for viewmodels." )
-    VMOffsetY = CreateClientConVar( "cfc_simple_weapons_vm_offset_y", 0, true, false, "The left/right offset to use for viewmodels." )
-    VMOffsetZ = CreateClientConVar( "cfc_simple_weapons_vm_offset_z", 0, true, false, "The up/down offset to use for viewmodelss." )
 end
