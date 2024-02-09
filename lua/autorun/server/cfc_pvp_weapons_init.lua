1b9639484caa508d50cce5167316eb9e6da222c7
legokidlogan <26103433+legokidlogan@users.noreply.github.com>
2024-02-09 15:51:25 -0700

    Mark for fastdl

--- lua/autorun/server/cfc_pvp_weapons_init.lua
+++ lua/autorun/server/cfc_pvp_weapons_init.lua
@@ -1,1 +1,32 @@
 resource.AddWorkshop( "3097864891" )
+
+
+-- TEMPORARY FASTDL CONTENT, REMOVE AFTER TESTING IS COMPLETE:
+resource.AddFile( "sound/weapons/cfc_bonk_shotgun/m3_insertshell.mp3" )
+resource.AddFile( "sound/weapons/cfc_bonk_shotgun/m3_pump.mp3" )
+resource.AddFile( "sound/weapons/cfc_bonk_shotgun/m3-1.wav" )
+
+resource.AddFile( "models/weapons/v_cfc_bonk_shotgun.mdl" )
+resource.AddFile( "models/weapons/w_cfc_bonk_shotgun.mdl" )
+
+resource.AddSingleFile( "materials/models/weapons/v_models/cfc_bonk_shotgun/12ga_g.vtf" )
+resource.AddSingleFile( "materials/models/weapons/v_models/cfc_bonk_shotgun/12ga_n.vtf" )
+resource.AddSingleFile( "materials/models/weapons/v_models/cfc_bonk_shotgun/12ga.vmt" )
+resource.AddSingleFile( "materials/models/weapons/v_models/cfc_bonk_shotgun/12ga.vtf" )
+resource.AddSingleFile( "materials/models/weapons/v_models/cfc_bonk_shotgun/actionbar_g.vtf" )
+resource.AddSingleFile( "materials/models/weapons/v_models/cfc_bonk_shotgun/actionbar_n.vtf" )
+resource.AddSingleFile( "materials/models/weapons/v_models/cfc_bonk_shotgun/actionbar.vmt" )
+resource.AddSingleFile( "materials/models/weapons/v_models/cfc_bonk_shotgun/actionbar.vtf" )
+resource.AddSingleFile( "materials/models/weapons/v_models/cfc_bonk_shotgun/ithaca_g.vtf" )
+resource.AddSingleFile( "materials/models/weapons/v_models/cfc_bonk_shotgun/ithaca_n.vtf" )
+resource.AddSingleFile( "materials/models/weapons/v_models/cfc_bonk_shotgun/ithaca.vtf" )
+resource.AddSingleFile( "materials/models/weapons/v_models/cfc_bonk_shotgun/m37.vmt" )
+resource.AddSingleFile( "materials/models/weapons/v_models/cfc_bonk_shotgun/shellholder_g.vtf" )
+resource.AddSingleFile( "materials/models/weapons/v_models/cfc_bonk_shotgun/shellholder_n.vtf" )
+resource.AddSingleFile( "materials/models/weapons/v_models/cfc_bonk_shotgun/shellholder.vmt" )
+resource.AddSingleFile( "materials/models/weapons/v_models/cfc_bonk_shotgun/shellholder.vtf" )
+
+resource.AddSingleFile( "materials/models/weapons/w_models/cfc_bonk_shotgun/12ga.vmt" )
+resource.AddSingleFile( "materials/models/weapons/w_models/cfc_bonk_shotgun/actionbar.vmt" )
+resource.AddSingleFile( "materials/models/weapons/w_models/cfc_bonk_shotgun/m37.vmt" )
+resource.AddSingleFile( "materials/models/weapons/w_models/cfc_bonk_shotgun/shellholder.vmt" )
