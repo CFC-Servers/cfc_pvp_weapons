dcd6f536a0dd3f4ba651f5cce35cf6dea4cc0a86
legokidlogan <26103433+legokidlogan@users.noreply.github.com>
2024-09-26 11:53:32 -0600

    Change mid-air split to a toggle on pressing r

--- lua/weapons/cfc_cluster_grenade/shared.lua
+++ lua/weapons/cfc_cluster_grenade/shared.lua
@@ -1,5 +1,7 @@
 AddCSLuaFile()
 
+DEFINE_BASECLASS( "cfc_simple_base_throwing" )
+
 if CLIENT then
     language.Add( "cfc_cluster_grenade_ammo", "Cluster Grenades" )
 end
@@ -27,36 +29,46 @@ SWEP.Primary = {
     LobAct = { ACT_VM_PULLBACK_LOW, ACT_VM_HAULBACK },
     RollAct = { ACT_VM_PULLBACK_LOW, ACT_VM_SECONDARYATTACK },
 
-    SplitDelay = 0.25, -- Primary fire only splits on impact, secondary splits either mid-air or on impact.
+    SplitDelay = 0.25, -- Reload toggles between only splitting on impact and splitting either mid-air or on impact.
 }
 
+SWEP.CFC_FirstTimeHints = {
+    {
+        Message = "Press reload (r) to toggle the grenades splitting mid-air.",
+        Sound = "ambient/water/drip1.wav",
+        Duration = 7,
+        DelayNext = 4,
+    },
+    {
+        Message = "If you time it right, it can be devastating.",
+        Sound = "ambient/water/drip2.wav",
+        Duration = 7,
+        DelayNext = 0,
+    },
+}
+
+
+function SWEP:SetupDataTables()
+    BaseClass.SetupDataTables( self )
+
+    self:AddNetworkVar( "Bool", "MidAirSplit" )
+end
 
 function SWEP:Initialize()
     self:SetMaterial( "models/weapons/w_models/cfc_frag_grenade/frag_grenade_cluster" )
 end
 
-function SWEP:PrimaryAttack()
-    if not self:CanThrow() then return end
+function SWEP:Reload()
+    if CLIENT then return end
 
-    self:SetThrowMode( 1 )
-    self:SetFinishThrow( CurTime() + self:SendTranslatedWeaponAnim( self.Primary.ThrowAct[1] ) )
-    self:SetNextIdle( 0 )
+    local nextReloadTime = self._getNextReloadTime or 0
+    local now = CurTime()
+    if nextReloadTime > now then return end
 
-    self:SetHoldType( self.HoldType )
+    self._getNextReloadTime = now + 0.5
 
-    self._cluster_splitMidair = false
-end
-
-function SWEP:SecondaryAttack()
-    if not self:CanThrow() then return end
-
-    self:SetThrowMode( 1 )
-    self:SetFinishThrow( CurTime() + self:SendTranslatedWeaponAnim( self.Primary.ThrowAct[1] ) )
-    self:SetNextIdle( 0 )
-
-    self:SetHoldType( self.HoldType )
-
-    self._cluster_splitMidair = true
+    self:SetMidAirSplit( not self:GetMidAirSplit() )
+    sound.Play( "doors/handle_pushbar_locked1.wav", self:GetOwner():EyePos(), 75, 130, 1 )
 end
 
 
@@ -71,10 +83,18 @@ if SERVER then
         ent:Spawn()
         ent:Activate()
 
-        if self._cluster_splitMidair then
+        if self:GetMidAirSplit() then
             ent:SetTimer( self.Primary.SplitDelay )
         end
 
         return ent
+    end
+else
+    function SWEP:CustomAmmoDisplay()
+        return {
+            Draw = true,
+            PrimaryClip = self:GetOwner():GetAmmoCount( self.Primary.Ammo ),
+            SecondaryAmmo = self:GetMidAirSplit() and 1 or nil,
+        }
     end
 end
