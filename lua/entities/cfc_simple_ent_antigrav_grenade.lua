cd2207691d5619de012c9021b86e59ec65aa20ac
legokidlogan <26103433+legokidlogan@users.noreply.github.com>
2024-09-27 14:49:20 -0600

    More antigrav visual/auditory effects

--- lua/entities/cfc_simple_ent_antigrav_grenade.lua
+++ lua/entities/cfc_simple_ent_antigrav_grenade.lua
@@ -101,6 +101,12 @@ function ENT:BubbleStartTouch( ent )
 
     ent._cfcPvPWeapons_AntiGravityGrenade = self
 
+    local rf = RecipientFilter()
+    rf:AddPlayer( ent )
+
+    sound.Play( "ambient/machines/machine1_hit2.wav", ent:EyePos(), 75, 120 )
+    util.ScreenShake( ent:EyePos(), 3, 5, 1.5, 500, true, ent )
+
     return true
 end
 
