7e8fc48e2dff38009d99f42a815e0e77e27b0c67
legokidlogan <26103433+legokidlogan@users.noreply.github.com>
2024-09-27 16:44:27 -0600

    Reduce discombob strength

--- lua/entities/cfc_simple_ent_discombob.lua
+++ lua/entities/cfc_simple_ent_discombob.lua
@@ -9,7 +9,7 @@ ENT.Model = Model( "models/weapons/w_eq_fraggrenade.mdl" )
 ENT.Damage = 100 -- Doesn't actually deal damage, just used to compare against damage falloff for scaling the knockback.
 ENT.Radius = 300
 ENT.Knockback = 1000 * 40
-ENT.PlayerKnockback = 600
+ENT.PlayerKnockback = 500
 ENT.PlayerSelfKnockback = 400
 ENT.PlayerKnockbackVelAdd = Vector( 0, 0, 200 )
 
