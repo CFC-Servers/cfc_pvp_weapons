b2ec6eedb10f300a23826e3b1f60a335d5e9f03b
legokidlogan <26103433+legokidlogan@users.noreply.github.com>
2025-07-27 14:30:13 -0500

    Style

--- lua/weapons/cfc_bonk_shotgun/shared.lua
+++ lua/weapons/cfc_bonk_shotgun/shared.lua
@@ -149,10 +149,9 @@ SWEP.Bonk = {
 
 
 function SWEP:EmitFireSound()
-    self:EmitSound( "npc/scanner/scanner_nearmiss1.wav", 80, 110, 1, CHAN_AUTO )
-
     self:EmitSound( "weapons/shotgun/shotgun_fire6.wav", 80, 105, 0.6, CHAN_WEAPON )
     self:EmitSound( "weapons/shotgun/shotgun_fire6.wav", 80, 110, 0.6, CHAN_AUTO )
+    self:EmitSound( "npc/scanner/scanner_nearmiss1.wav", 80, 110, 1, CHAN_AUTO )
 
     timer.Create( "CFCBonkShotgun_CockSound_" .. self:EntIndex(), 0.3, 1, function()
         if not IsValid( self ) then return end
