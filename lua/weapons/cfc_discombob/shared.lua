37e0e6ae614cf115f6d1900b255a4f7f0918391e
legokidlogan <26103433+legokidlogan@users.noreply.github.com>
2026-02-19 12:41:57 -0600

    Allow discombob to be detonated manually

--- lua/weapons/cfc_discombob/shared.lua
+++ lua/weapons/cfc_discombob/shared.lua
@@ -25,7 +25,7 @@ SWEP.ThrowingHoldType = "melee"
 
 SWEP.Primary = {
     Ammo = "cfc_discomob",
-    DefaultClip = 1,
+    DefaultClip = 50,
 
     ThrowAct = { ACT_VM_PULLBACK_HIGH, ACT_VM_THROW },
     LobAct = { ACT_VM_PULLBACK_LOW, ACT_VM_HAULBACK },
@@ -36,10 +36,87 @@ SWEP.Primary = {
     StrengthMultSelf = 1.25, -- Affects the knockback strength against the player who threw it
 }
 
+SWEP.ThrowCooldown = 0 -- Normal grenade cooldown on throw. Viewmodel anims and the nade being undetonated are separate cooldowns.
+SWEP.PostDetCooldown = 1 -- Unique param for the discombob; applies after the previous nade explodes.
+
+SWEP.CFC_FirstTimeHints = {
+    {
+        Message = "The Discombob deals no damage, but pushes people around.",
+        Sound = "ambient/water/drip1.wav",
+        Duration = 10,
+        DelayNext = 6,
+    },
+    {
+        Message = "Attack again or reload after throwing the Discombob to detonate it early.",
+        Sound = "ambient/water/drip2.wav",
+        Duration = 7,
+        DelayNext = 0,
+    },
+}
+
+
+function SWEP:SetupDataTables()
+    BaseClass.SetupDataTables( self )
+
+    self:AddNetworkVar( "Bool", "WaitingForNadeDet" )
+end
 
 function SWEP:Initialize()
     BaseClass.Initialize( self )
     self:SetMaterial( "models/weapons/w_models/cfc_frag_grenade/frag_grenade_discombob" )
+end
+
+function SWEP:CanThrow()
+    if self:GetWaitingForNadeDet() then return false end
+
+    return BaseClass.CanThrow( self )
+end
+
+function SWEP:Throw()
+    if CLIENT then
+        self._discombobPlayedManualDetSound = nil
+    end
+
+    return BaseClass.Throw( self )
+end
+
+function SWEP:Reload()
+    self:TryManualDetonation()
+end
+
+function SWEP:PrimaryAttack()
+    self:TryManualDetonation()
+
+    return BaseClass.PrimaryAttack( self )
+end
+
+function SWEP:SecondaryAttack()
+    self:TryManualDetonation()
+
+    return BaseClass.SecondaryAttack( self )
+end
+
+function SWEP:TryManualDetonation()
+    if not self:GetWaitingForNadeDet() then return end
+
+    local playDetSound = false
+
+    if SERVER then
+        local nade = self._discombobNadeEnt
+
+        if IsValid( nade ) then
+            playDetSound = true
+            self._discombobNadeEnt = nil
+            nade:Explode()
+        end
+    elseif not self._discombobPlayedManualDetSound then
+        playDetSound = true
+        self._discombobPlayedManualDetSound = true
+    end
+
+    if playDetSound then
+        self:EmitSound( "weapons/slam/buttonclick.wav" )
+    end
 end
 
 
@@ -64,6 +141,23 @@ if SERVER then
         ent.Knockback = ent.Knockback * strengthMult
         ent.PlayerKnockback = ent.PlayerKnockback * strengthMult
         ent.PlayerSelfKnockback = ent.PlayerSelfKnockback * strengthMultSelf
+
+        self:SetWaitingForNadeDet( true )
+        self._discombobNadeEnt = ent
+
+        local selfObj = self
+
+        ent:CallOnRemove( "CFC_PvPWeapons_Discombob_TrackDetonation", function()
+            if not IsValid( selfObj ) then return end
+
+            selfObj._discombobNadeEnt = nil
+
+            timer.Create( "CFC_PvPWeapons_Discombob_Cooldown" .. selfObj:EntIndex(), selfObj.PostDetCooldown, 1, function()
+                if not IsValid( selfObj ) then return end
+
+                selfObj:SetWaitingForNadeDet( false )
+            end )
+        end )
 
         return ent
     end
